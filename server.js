30f05791a3e974c9030201f377b9671499db48a1
peta swan <petaswan986@gmail.com>
2023-11-03 20:19:35 +1100

    code update. notes.html has been connected

--- server.js
+++ server.js
@@ -1,14 +1,23 @@
 const express = require('express');
-
+const db = require('./db/db.json')
 const app = express()
+const api = require()
 
 const port = 3001
 
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
+    res.json(db)
+})
 
-});
+app.get('/notes', (req, res) => {
+    res.sendFile('/Users/peta_/bootcamp/Note-Taker-App/public/notes.html')
+})
+
+app.post('/notes', (req, res) => {
+
+})
 
 app.listen(port, () => {
     console.log("listening on port 3001");
